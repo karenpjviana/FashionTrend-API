cdb553b515c8eaba580ef2900e009d1b0298318a
Karen Viana <karenpjvianaa@gmail.com>
2023-12-03 07:18:07 -0300

    Adicionando documentação

--- FashionTrend/FashionTrend.API/Program.cs
+++ FashionTrend/FashionTrend.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 namespace FashionTrend.API
 {
     public class Program
@@ -12,10 +15,33 @@ namespace FashionTrend.API
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddLogging(logging =>
+            {
+                logging.AddConsole();
+                logging.AddDebug();
+            });
+            
+            builder.Services.AddSwaggerGen(options =>
+            {
+                var openApiInfo = new OpenApiInfo();
+
+                openApiInfo.Title = "API FashionTrend - Grupo de Sustentabilidade";
+                openApiInfo.Description = "Documentação da API FashionTrend para o módulo de Arquitetura de software 2 do Grupo de Sustentabilidade, oferece a funcionalidade para costureiras(fornecedores) se cadastrarem e acessarem oportunidades de serviço disponibilizadas pela Fashion Trend.";
+
+                openApiInfo.Contact = new OpenApiContact()
+                {
+                    Name = "Turma 1038"
+                };
+
+                options.SwaggerDoc("v1", openApiInfo);
+
+                var fileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var path = Path.Combine(AppContext.BaseDirectory, fileName);
+                options.IncludeXmlComments(path, true);
+            });
 
             // Add services to the container.
-
             builder.Services.AddControllers();
 
             builder.Services.ConfigureCorsPolicy();
